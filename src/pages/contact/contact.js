8e54a365e01a0e0d432e048163daa53f04bd82cc
Kristofer Fagerström <kristofer.fagerstrom@mtgx.se>
2018-11-06 21:44:14 +0100

    Add some styling to the contact page

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import KeyValue from '../../components/keyValue/keyValue';
 import type { Address } from './contactReducer';
-import { Paper, withStyles } from '@material-ui/core';
+import { Paper, withStyles, Grid } from '@material-ui/core';
 
 type Props = {
   address: Address,
@@ -10,25 +10,41 @@ type Props = {
 
 const styles = theme => ({
   root: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
+    flexGrow: 1,
+    padding: theme.spacing.unit * 2,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    color: theme.palette.text.secondary,
   },
 });
-
 export class Contact extends Component<Props> {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        <Paper>
-          <div>Contact information</div>
-          <KeyValue description="Address" value={this.props.address.street} />
-          <KeyValue
-            description="Postnummer"
-            value={this.props.address.areaCode}
-          />
-          <KeyValue description="Stad" value={this.props.address.city} />
-        </Paper>
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <div>Contact information</div>
+              <KeyValue
+                description="Address"
+                value={this.props.address.street}
+              />
+              <KeyValue
+                description="Postnummer"
+                value={this.props.address.areaCode}
+              />
+              <KeyValue description="Stad" value={this.props.address.city} />
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <div>Blank paper</div>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
