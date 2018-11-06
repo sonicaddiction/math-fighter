4075c830b0da8df6b0e22ca069b444995df6a9c1
Kristofer Fagerström <kristofer.fagerstrom@mtgx.se>
2018-11-06 21:48:09 +0100

    Add styling to home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { withStyles, Grid, Paper } from '@material-ui/core';
 
 type Props = {};
 
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 2,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    color: theme.palette.text.secondary,
+  },
+});
+
 class Home extends Component<Props> {
   render() {
+    const { classes } = this.props;
+
     return (
-        <div>Home page</div>
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>Home page</Paper>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
