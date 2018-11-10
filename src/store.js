c8c6d887dadb49029c6e27b5d9f92292375885a5
Kristofer Fagerström <kristofer.fagerstrom@mtgx.se>
2018-11-10 10:18:52 +0100

    Add redux dev tool support

--- src/store.js
+++ src/store.js
@@ -2,14 +2,18 @@ import { combineReducers, createStore } from 'redux';
 import { contactReducer } from './pages/contact/contactReducer';
 
 export const reducers = combineReducers({
-  contact: contactReducer
+  contact: contactReducer,
 });
 
 export type RootState = {};
 
 // store.js
 export function configureStore(initialState: RootState = {}) {
-  const store = createStore(reducers, initialState);
+  const store = createStore(
+    reducers,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
   return store;
 }
 
